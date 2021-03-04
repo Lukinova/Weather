f2d9fa80b4bbdd5146fb58fe0cbc4dd188942710
Lukinova <evil1555fox@gmail.com>
2021-03-05 00:58:23 +0300

    прогноз погоды для все мира в реальном времени

--- api.php
+++ api.php
@@ -1,5 +1,8 @@
 <?php
-$apiResult = file_get_contents('http://api.openweathermap.org/data/2.5/weather?q=Saint%20Petersburg,ru&appid=f2d6eac751834befaefa082be2133dff');
+ini_set('display_errors', 'On');
+$apiKey = 'f2d6eac751834befaefa082be2133dff'; 
+$city = (isset($_GET['city'])) ? $_GET['city'] : null;
+$apiResult = file_get_contents('http://api.openweathermap.org/data/2.5/weather?q='. (($city) ? $city : ('Saint Petersburg')) .'&appid='.$apiKey);
 $apiResult = json_decode($apiResult);
 $name = ($apiResult->name);
 $description = ($apiResult->weather[0]->description);
@@ -11,23 +14,17 @@ $humidity = ($apiResult->main->humidity);
 $clouds = ($apiResult->clouds->all);
 $sunrise = date('h:i:s' ,$apiResult->sys->sunrise);
 $sunset = date('h:i:s' ,$apiResult->sys->sunset);
-
-$api_five_days = file_get_contents('http://api.openweathermap.org/data/2.5/forecast?q=Sant%20Petersburg&appid=f2d6eac751834befaefa082be2133dff');
-$api_five_days = json_decode($api_five_days);
-$one_day = date('F j' ,$api_five_days->list[0]->dt);
-$two_day = date('F j' ,$api_five_days->list[8]->dt);
-$three_day =  date('F j' ,$api_five_days->list[16]->dt);
-$thour_day = date('F j' ,$api_five_days->list[24]->dt);
-$five_day = date('F j' ,$api_five_days->list[32]->dt);
-
-$temp_one = (round($api_five_days->list[0]->main->temp - 273));
-$temp_two = (round($api_five_days->list[8]->main->temp - 273));
-$temp_three = (round($api_five_days->list[16]->main->temp - 273));
-$temp_thour = (round($api_five_days->list[24]->main->temp - 273));
-$temp_five = (round($api_five_days->list[32]->main->temp - 273));
-
-$temp_feels_one = (round($api_five_days->list[0]->main->feels_like - 273));
-$temp_feels_two = (round($api_five_days->list[8]->main->feels_like - 273));
-$temp_feels_three = (round($api_five_days->list[16]->main->feels_like - 273));
-$temp_feels_thour = (round($api_five_days->list[24]->main->feels_like - 273));
-$temp_feels_five = (round($api_five_days->list[32]->main->feels_like - 273));
+$icon = ($apiResult->weather[0]->icon);
+?>
+<ul>
+	<span class="city_name"><?php echo $name?></span>
+	<li><img src="http://openweathermap.org/img/wn/<?=$apiResult->weather[0]->icon;?>@2x.png"/><?php echo $description;?></li>
+	<li>Now:<?php echo $temp?>℃</li>
+	<li>Feels like:<?php echo $feels_like?>℃</li>
+	<li>Wind speed:<?php echo $wind_speed?>m/s</li>
+	<li>Pressure:<?php echo $pressure?>mbar</li>
+	<li>Humidity:<?php echo $humidity?>%</li>
+	<li>Clouds:<?php echo $clouds?>%</li>
+	<li>Sunrise:<?php echo $sunrise?>am</li>
+	<li>Sunset:<?php echo $sunset?>pm</li>
+</ul>
--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 	<title>Weather</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=PT+Sans&display=swap" rel="stylesheet">
@@ -11,64 +13,33 @@
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
 </head>
 <body>
-	<?php
-	include_once 'api.php'
-	?>
-	<div class="selected_city">
-		<div class="flex-container">
-				<div class="yestaday_sity_weather">
-				<ul>
-					<span class="city"><?php echo $name;?></span>
-					<li><img src="http://openweathermap.org/img/wn/<?=$apiResult->weather[0]->icon;?>@2x.png"/><?php echo $description;?></li>
-					<li>Now: <?php echo $temp;?> °C</li>
-					<li>Feels: <?php echo $feels_like;?>°C</li>
-					<li>Wind: <?php echo $wind_speed;?> m/s</li>
-					<li>Pressure: <?php echo $pressure;?>mbar</li>
-					<li>Humidity: <?php echo $humidity;?>%</li>
-					<li>Cloudy: <?php echo $clouds;?>%</li>
-					<li>Sunrise: <?php echo $sunrise;?>am <img src="https://img.icons8.com/color/48/000000/morning.png"/></li>
-					<li>Sunset: <?php echo $sunset;?>pm <img src="https://img.icons8.com/color/48/000000/evening.png"/></li>
-
-				</ul>
-			</div>
-			<div class="future_forecast">
-				<div class="one_day">
-					<span class="date"><?php echo $one_day; ?></span>
-						<ul class="forecast">
-							<li class="forecast">Now: <?php echo $temp_one;?>℃</li>
-							<li>Feels: <?php echo $temp_feels_one?>℃</li>
-						</ul>
-				</div>
-				<div class="two_day">
-					<span class="date"><?php echo $two_day; ?></span>
-						<ul class="forecast">
-							<li>Now: <?php echo $temp_two;?>℃</li>
-							<li>Feels: <?php echo $temp_feels_two?>℃</li>
-						</ul>
-				</div>
-				<div class="three_day">
-					<span class="date"><?php echo $three_day; ?></span>
-						<ul class="forecast">
-							<li>Now: <?php echo $temp_three;?>℃</li>
-							<li>Feels: <?php echo $temp_feels_three?>℃</li>
-						</ul>
-				</div>
-				<div class="thour_day">
-					<span class="date"><?php echo $thour_day; ?></span>
-						<ul class="forecast">
-							<li>Now: <?php echo $temp_thour;?>℃</li>
-							<li>Feels: <?php echo $temp_feels_thour?>℃</li>
-						</ul>
-				</div>
-				<div class="five_day">
-					<span class="date"><?php echo $five_day; ?></span>
-						<ul class="forecast">
-							<li>Now: <?php echo $temp_five;?>℃</li>
-							<li>Feels: <?php echo $temp_feels_five?>℃</li>
-						</ul>
-				</div>
-			</div>
+	<div class="container">
+		<div class="now_forecast"></div>
+		<div class="city_selection">
+			<span class="city_selection">What city do you want to visit?</span>
+			<form action="/index.php">
+				<input type="text" name="city" autofocus="autofocus">
+				<input type="submit" class="submit" value="Select"></input>
+			</form>		
 		</div>
-	</div>
+</div>
+<script
+  src="https://code.jquery.com/jquery-3.6.0.min.js"
+  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+  crossorigin="anonymous"></script>
+<script type="text/javascript">
+	$('form').on('submit', function () {
+    $.get('/api.php', {'city': $('input[name="city"]').val()}).done(function(data){
+       console.log(data);
+       $('.container > .city_selection').slideToggle();
+       $('.now_forecast').html(data);
+       setTimeout(function(){$('.now_forecast').slideToggle();}, 1000);
+    });
+    return false;
+});
+$(document).ready(function() {
+    $('input[name="city"]').focus();
+});
+</script>
 </body>
 </html>
